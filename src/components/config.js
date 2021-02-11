5ad5175bfdc4f25481cb22ea8d83fc6fb2ec7604
Victor Chan <victorcnk@gmail.com>
2021-02-11 23:25:51 +0800

    changed endpoint

--- src/components/config.js
+++ src/components/config.js
@@ -4,6 +4,7 @@ console.log(process.env)
 
 export default {
     //API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
-    API_ENDPOINT: `http://127.0.0.1:9090`,
+    //API_ENDPOINT: `http://127.0.0.1:9090`,
+    API_ENDPOINT: `https://whispering-taiga-96894.herokuapp.com/`,
     API_KEY: process.env.REACT_APP_API_KEY || `b7ff46d2-36ab-4318-bef8-6a76412ce160`
   }
